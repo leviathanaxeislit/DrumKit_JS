efde1092eb7f32f9b8b452a4f0a4086403e39613
Chandra Prakash <scp.thedevil@gmail.com>
2022-07-13 09:06:08 +0000

    Switch Sound assets to CDN
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ document.querySelector(".random-beat").addEventListener("click", function () {
     const randomNoteArrayIndex = Math.floor(
       Math.random() * (numberOfDrumButtons + 1)
     );
-    const selectedNote = document.querySelectorAll("button")[
-      randomNoteArrayIndex
-    ];
+    const selectedNote =
+      document.querySelectorAll("button")[randomNoteArrayIndex];
 
     makeSound(selectedNote.innerHTML);
     buttonAnimation(selectedNote.innerHTML);
 
     numberOfRandomBeat--;
-    
+
     if (numberOfRandomBeat === 0) {
       clearInterval(randomPlayer);
       this.classList.remove("pressed-beat-generator");
@@ -24,82 +23,85 @@ document.querySelector(".random-beat").addEventListener("click", function () {
 });
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("mouseover", function () {
+      let buttonInnerHTML = this.innerHTML;
 
-  document.querySelectorAll(".drum")[i].addEventListener("mouseover", function() {
+      makeSound(buttonInnerHTML);
 
-    let buttonInnerHTML = this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-
-    buttonAnimation(buttonInnerHTML);
-
-  });
-
+      buttonAnimation(buttonInnerHTML);
+    });
 }
 
-document.addEventListener("keypress", function(event) {
-
+document.addEventListener("keypress", function (event) {
   makeSound(event.key);
 
   buttonAnimation(event.key);
-
 });
 
-
 function makeSound(key) {
-
   switch (key) {
     case "w":
-      let tom1 = new Audio("sounds/w.mp3");
+      let tom1 = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/w.mp3"
+      );
       tom1.play();
       break;
 
     case "a":
-      let tom2 = new Audio("sounds/a.mp3");
+      let tom2 = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/a.mp3"
+      );
       tom2.play();
       break;
 
     case "s":
-      let tom3 = new Audio('sounds/s.mp3');
+      let tom3 = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/s.mp3"
+      );
       tom3.play();
       break;
 
     case "d":
-      let tom4 = new Audio('sounds/d.mp3');
+      let tom4 = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/d.mp3"
+      );
       tom4.play();
       break;
 
     case "j":
-      let snare = new Audio('sounds/j.mp3');
+      let snare = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/j.mp3"
+      );
       snare.play();
       break;
 
     case "k":
-      let crash = new Audio('sounds/k.mp3');
+      let crash = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/k.mp3"
+      );
       crash.play();
       break;
 
     case "l":
-      let kick = new Audio('sounds/l.mp3');
+      let kick = new Audio(
+        "https://cdn.glitch.global/8075e640-598c-43fe-9ffd-f3766cecb71d/l.mp3"
+      );
       kick.play();
       break;
 
-
     default:
       console.log(key);
-
   }
 }
 
 function buttonAnimation(currentKey) {
-
   let activeButton = document.querySelector("." + currentKey);
 
   activeButton.classList.add("pressed");
 
-  setTimeout(function() {
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
-
